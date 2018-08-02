9acbb7162cb8af6a1ceacab8cfa592f72e85a7e8
eswilson <eswilson4@gmail.com>
2018-08-02 07:50:31 -0700

    forgot about commits. Made psychic game

---
+++ assets/javascript/game.js
@@ -0,0 +1,71 @@
+// Psychic Game
+// Picks a letter, and user tries to guess the letter
+// Global variables
+var wins = 0;
+var losses = 0;
+var lives = 9;
+var guessArray = [];
+
+
+// Function for updating wins, losses, and lives
+function updateStats() {
+  document.querySelector("#wins").innerHTML = "Wins: " + wins;
+  document.querySelector("#losses").innerHTML = "Losses: " + losses;
+  document.querySelector("#lives").innerHTML = "Lives: " + lives;
+  document.querySelector("#guesses").innerHTML = "Your guesses: " + guessArray.join(', ');
+};
+
+// When a player loses a round, reset lives to 9
+function endRound() {
+  lives = 9;
+  guessArray = [];
+};
+
+var answer = String.fromCharCode(97+Math.floor(Math.random() * 26));
+
+updateStats();
+
+document.onkeyup = function (event) {
+  // Variable for user key events
+  var guess = event.key;
+
+  // Function for generating a new value for answer
+  function newLetter() {
+  answer = String.fromCharCode(97+Math.floor(Math.random() * 26));
+};
+  
+  if (guess >= "a" && guess <= "z" && lives > 1) {
+    if (guess === answer) {
+      wins++;
+      endRound();
+      updateStats();
+      newLetter();
+      console.log("guess: " + guess, "answer: " + answer)
+    }
+    else {
+      lives--;
+      guessArray.push(guess);
+      updateStats();
+      console.log("guess: " + guess, "answer: " + answer)
+    }
+  
+  }
+
+  else if (guess >= "a" && guess <= "z" && lives === 1) {
+    if (guess === answer) {
+      wins++;
+      endRound();
+      updateStats();
+      newLetter();
+      console.log("guess: " + guess, "answer: " + answer)
+    }
+    else {
+      losses++;
+      endRound();
+      updateStats();
+      newLetter();
+      console.log("guess: " + guess, "answer: " + answer)
+    }
+  }
+
+};
